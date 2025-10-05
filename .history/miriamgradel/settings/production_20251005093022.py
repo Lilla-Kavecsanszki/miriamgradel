e3f21fa4a22084d31e8d4b01552556afc48246c4
Lilla Kavecsanszki <lilla-kavecsanszki@outlook.com>
2025-10-05 09:37:55 +0200

    Updated production.py for static files pick up

--- .history/miriamgradel/settings/production_20251005093022.py
+++ .history/miriamgradel/settings/production_20251005093022.py
@@ -2,11 +2,17 @@ from .base import *
 
 DEBUG = False
 
+try:
+    from .local import *
+except ImportError:
+    pass
+
+
 # Force manifest storage in prod (order-proof)
 STORAGES["staticfiles"] = {
     "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
 }
 
-# (Optional safety) ensure WhiteNoise middleware is present early
+# (Optional safety) ensure middleware present
 if "whitenoise.middleware.WhiteNoiseMiddleware" not in MIDDLEWARE:
     MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
